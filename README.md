a97527d2c2f9f799be2d90f184f43e182ee30058
Jesse Cox <jess.coxpdx@gmail.com>
2021-07-15 07:41:40 -0700

    full commit 2

--- README.md
+++ README.md
@@ -1,6 +1,21 @@
-# node-red-contrib-scale
-[Node-RED](http://nodered.org/) node to control the WAGO PFC I/O bus
+@prescient-devices/node-red-node-mqtt-in-json
+=============================================
 
-##Version history
-* 0.0.1	First release
-  * Tested 07/12/2021
+MQTT input node that JSON-parses payload possible, and optionally ignores empty
+topics.  Paired with
+[@prescient-devices/node-red-node-mqtt-out-json](https://flows.nodered.org/node/@prescient-devices/node-red-node-mqtt-out-json)
+it is able to receive a whole message, not just the payload.
+
+Copyright 2020 Prescient Devices, Inc.
+
+Permission to use, copy, modify, and/or distribute this software for any
+purpose with or without fee is hereby granted, provided that the above
+copyright notice and this permission notice appear in all copies.
+
+THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND
+FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+PERFORMANCE OF THIS SOFTWARE.
---
+++ mqtt-in-json.js
@@ -0,0 +1,90 @@
+module.exports = function (RED) {
+  "use strict"
+  var isUtf8 = require("is-utf8")
+
+  function MQTTInJSONNode(n) {
+    RED.nodes.createNode(this, n)
+    this.topic = n.topic.trim()
+    this.qos = parseInt(n.qos)
+    if (isNaN(this.qos) || this.qos < 0 || this.qos > 2) {
+      this.qos = 2
+    }
+    this.broker = n.broker
+    this.brokerConn = RED.nodes.getNode(this.broker)
+    this.ignoreEmpty = n.ignoreempty
+    this.removeMqttConfig = n.removemqttconfig
+    if (!/^(#$|(\+|[^+#]*)(\/(\+|[^+#]*))*(\/(\+|#|[^+#]*))?$)/.test(this.topic)) {
+      return this.warn(RED._("mqtt.errors.invalid-topic"))
+    }
+    var node = this
+    if (this.brokerConn) {
+      this.status({
+        fill: "red",
+        shape: "ring",
+        text: "node-red:common.status.disconnected",
+      })
+      if (this.topic) {
+        node.brokerConn.register(this)
+        this.brokerConn.subscribe(
+          this.topic,
+          this.qos,
+          function (topic, payload, packet) {
+            if (isUtf8(payload)) {
+              try {
+                payload = JSON.parse(payload.toString())
+              } catch (err) {
+                payload = payload.toString()
+              }
+            }
+            var msg = {
+              topic: topic,
+              payload: payload,
+              qos: packet.qos,
+              retain: packet.retain,
+            }
+            if (payload === Object(payload) && "_payload" in payload) {
+              for (const prop of Object.keys(payload._payload)) {
+                msg[prop] = payload._payload[prop]
+              }
+              delete payload._payload
+            }
+            if (
+              node.brokerConn.broker === "localhost" ||
+              node.brokerConn.broker === "127.0.0.1"
+            ) {
+              msg._topic = topic
+            }
+            if (this.removeMqttConfig) {
+              var delList = ["_topic", "qos", "retain", "topic"]
+              for (const item of delList) {
+                if (item in msg) {
+                  delete msg[item]
+                }
+              }
+            }
+            node.send(msg)
+          }.bind(this),
+          this.id
+        )
+        if (this.brokerConn.connected) {
+          node.status({
+            fill: "green",
+            shape: "dot",
+            text: "node-red:common.status.connected",
+          })
+        }
+      } else if (!this.ignoreEmpty) {
+        this.error(RED._("mqtt.errors.not-defined"))
+      }
+      this.on("close", function (done) {
+        if (node.brokerConn) {
+          node.brokerConn.unsubscribe(node.topic, node.id)
+          node.brokerConn.deregister(node, done)
+        }
+      })
+    } else {
+      this.error(RED._("mqtt.errors.missing-config"))
+    }
+  }
+  RED.nodes.registerType("wago kbus", MQTTInJSONNode)
+}
