4f003b6668e5edb299e77e32e7ec9eafef2a85db
Jesse Cox <jess.coxpdx@gmail.com>
2021-07-15 13:22:26 -0700

    Update kbus_tc.js

--- wago/kbus_tc.js
+++ wago/kbus_tc.js
@@ -1,37 +1,39 @@
-module.exports = function(RED) {
-    "use strict";
+module.exports = function (RED) {
+  "use strict";
 
-    function tempInput(n) {
-       RED.nodes.createNode(this,n);
-       //var context = this.context();
-       var node = this;
-       var module_num = n.module;
-       var channel_num = n.channel;
-       var signal_type = n.signal_type;     
+  function toFixed(num, precision) {
+    return (+(Math.round(+(num + "e" + precision)) + "e" + -precision)).toFixed(precision);
+  }
 
-       function toFixed( num, precision ) {
-        return (+(Math.round(+(num + 'e' + precision)) + 'e' + -precision)).toFixed(precision);
+  function tempInput(n) {
+    RED.nodes.createNode(this, n);
+    //var context = this.context();
+    var node = this;
+    var module_num = n.module;
+    var channel_num = n.channel;
+    var signal_type = n.signal_type;
+
+    this.on("input", function (msg) {
+      var outputMsg = {};
+      var outValue = 0;
+      var actual_module_num = msg.payload.payload.module;
+      var actual_channel_num = msg.payload.payload.channel;
+      var raw_input = msg.payload.payload.value;
+
+      if (
+        actual_channel_num == channel_num &&
+        actual_module_num == module_num
+      ) {
+        if (signal_type == "Celsius") {
+          outValue = toFixed(raw_input / 10, 2);
         }
-
-        this.on('input', function(msg) {
-            var outputMsg = {};
-            var outValue = 0;
-            var actual_module_num = msg.payload.payload.module;
-            var actual_channel_num = msg.payload.payload.channel;
-            var raw_input = msg.payload.payload.value;
-
-
-            if ((actual_channel_num == channel_num) && (actual_module_num == module_num)) {
-                if (signal_type == "Celsius")    {
-                    outValue = toFixed((raw_input / 10), 2);
-                }
-                if (signal_type == "Farenheit")  {
-                    outValue = toFixed((raw_input / 10 * (9/5) + 32), 2);              
-                }    
-                outputMsg.payload = parseFloat(outValue);       
-                node.send(outputMsg);
-            }
-        });
-    }
-    RED.nodes.registerType("Temperature Input",tempInput);
+        if (signal_type == "Farenheit") {
+          outValue = toFixed((raw_input / 10) * (9 / 5) + 32, 2);
+        }
+        outputMsg.payload = parseFloat(outValue);
+        node.send(outputMsg);
+      }
+    });
+  }
+  RED.nodes.registerType("Temperature Input", tempInput);
 };
