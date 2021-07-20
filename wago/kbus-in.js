c507a34b463e8b27664f725aab8694240976cf18
Jesse Cox <jess.coxpdx@gmail.com>
2021-07-20 16:14:20 -0700

    Update kbus-in.js

--- wago/kbus-in.js
+++ wago/kbus-in.js
@@ -68,6 +68,18 @@ module.exports = function (RED) {
               }
             }
             if (msg.topic == status_topic)  {
+              if (msg.payload.switch_state == "STOP") {
+                node.status({
+                  fill: "yellow",
+                  shape: "dot",
+                  text: "node-red:common.status.connected" + "|| switch: stop",
+                })
+              } else  {
+                node.status({
+                  fill: "green",
+                  shape: "dot",
+                  text: "node-red:common.status.connected" + "|| switch: run",
+              }
               node.send([msg, null]);
             }
             if (msg.topic == pi_topic)  {
