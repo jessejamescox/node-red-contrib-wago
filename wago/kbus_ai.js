942f90dda31714113996848aac251dd2c9bdeb32
Jesse Cox <jess.coxpdx@gmail.com>
2021-08-02 15:25:00 -0700

    Update kbus_ai.js

--- wago/kbus_ai.js
+++ wago/kbus_ai.js
@@ -55,6 +55,7 @@ module.exports = function (RED) {
             if (msg.payload.state.reported.controller.modules.hasOwnProperty(moduleStr)) {
                 if (msg.payload.state.reported.controller.modules[moduleStr].channels.hasOwnProperty(channelStr)) {
 
+                    // copy in the channel value
                     var rawInput = msg.payload.state.reported.controller.modules[moduleStr].channels[channelStr].value;
 
                     switch (resolution) {
